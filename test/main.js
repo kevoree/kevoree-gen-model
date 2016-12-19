624c5edd0d76129b8e7edd1c71deb0f9ab5eed0f
Maxime Tricoire <max.tricoire@gmail.com>
2016-12-19 16:18:58 +0100

    v3.0.18: update to kevoree-hash@^2.0.0

--- test/main.js
+++ test/main.js
@@ -6,19 +6,20 @@ var path = require('path');
 var modelGen = require('../kevoree-gen-model');
 
 describe('kevoree-gen-model tests', function () {
-    this.timeout(2000);
+	this.timeout(2000);
 
-    it('generate examples/fakecomp model', function (done) {
-        var modulePath = path.resolve(__dirname, '..', 'examples', 'fakecomp');
-        modelGen(modulePath, true, function (err) {
-            done(err);
-        });
-    });
+	it('generate examples/fakecomp model', function(done) {
+		var modulePath = path.resolve('test', 'fixtures', 'fakecomp');
+		modelGen(modulePath, true, function (err) {
+			// TODO read generated model and compare results
+			done(err);
+		});
+	});
 
-    it('generate examples/fakenode model', function (done) {
-        var modulePath = path.resolve(__dirname, '..', 'examples', 'fakenode');
-        modelGen(modulePath, true, function (err) {
-            done(err);
-        });
-    });
+	it('generate examples/fakenode model', function(done) {
+		var modulePath = path.resolve('test', 'fixtures', 'fakenode');
+		modelGen(modulePath, true, function (err) {
+			done(err);
+		});
+	});
 });
